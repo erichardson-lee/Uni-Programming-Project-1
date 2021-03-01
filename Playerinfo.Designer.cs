2a86bca9ce0ea36248ef2503616e2d8ca833c5d4
Edward Richardson-lee <erichardson-lee@fairfields.co.uk>
2021-03-01 12:11:26 +0000

    Player Info

---
+++ Playerinfo.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace O_Neillo
+{
+    partial class Playerinfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.presentTokens = new System.Windows.Forms.Label();
+            this.playername = new System.Windows.Forms.TextBox();
+            this.img_playerIcon = new System.Windows.Forms.PictureBox();
+            this.CurrentPlayer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.img_playerIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CurrentPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // presentTokens
+            // 
+            this.presentTokens.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.presentTokens.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.presentTokens.Location = new System.Drawing.Point(3, 3);
+            this.presentTokens.Margin = new System.Windows.Forms.Padding(3);
+            this.presentTokens.Name = "presentTokens";
+            this.presentTokens.Size = new System.Drawing.Size(92, 81);
+            this.presentTokens.TabIndex = 0;
+            this.presentTokens.Text = "0x";
+            this.presentTokens.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // playername
+            // 
+            this.playername.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.playername.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.playername.Location = new System.Drawing.Point(151, 40);
+            this.playername.Name = "playername";
+            this.playername.Size = new System.Drawing.Size(175, 44);
+            this.playername.TabIndex = 1;
+            // 
+            // img_playerIcon
+            // 
+            this.img_playerIcon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.img_playerIcon.Location = new System.Drawing.Point(101, 40);
+            this.img_playerIcon.Name = "img_playerIcon";
+            this.img_playerIcon.Size = new System.Drawing.Size(44, 44);
+            this.img_playerIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_playerIcon.TabIndex = 2;
+            this.img_playerIcon.TabStop = false;
+            // 
+            // CurrentPlayer
+            // 
+            this.CurrentPlayer.Image = global::O_Neillo.Properties.Resources.Current_Player;
+            this.CurrentPlayer.Location = new System.Drawing.Point(151, 3);
+            this.CurrentPlayer.Name = "CurrentPlayer";
+            this.CurrentPlayer.Size = new System.Drawing.Size(175, 31);
+            this.CurrentPlayer.TabIndex = 3;
+            this.CurrentPlayer.TabStop = false;
+            this.CurrentPlayer.Visible = false;
+            // 
+            // Playerinfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.CurrentPlayer);
+            this.Controls.Add(this.img_playerIcon);
+            this.Controls.Add(this.playername);
+            this.Controls.Add(this.presentTokens);
+            this.Name = "Playerinfo";
+            this.Size = new System.Drawing.Size(329, 87);
+            ((System.ComponentModel.ISupportInitialize)(this.img_playerIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CurrentPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label presentTokens;
+        private System.Windows.Forms.TextBox playername;
+        private System.Windows.Forms.PictureBox img_playerIcon;
+        private System.Windows.Forms.PictureBox CurrentPlayer;
+    }
+}
---
+++ Playerinfo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace O_Neillo
+{
+    public partial class Playerinfo : UserControl
+    {
+
+
+        private int _tokens;
+        [Description("Current number of tokens held by the player"), Category("O'Neillo")]
+        public int Tokens
+        {
+            get
+            {
+                return _tokens;
+            }
+            set
+            {
+                _tokens = value;
+                presentTokens.Text = $"{_tokens}x";
+            }
+        }
+
+        public Image _playerIcon;
+        [Description("Icon for this player"), Category("O'Neillo")]
+        public Image PlayerIcon
+        {
+            get
+            {
+                return _playerIcon;
+            }
+            set
+            {
+                _playerIcon = value;
+                img_playerIcon.Image = value;
+            }
+        }
+
+        public string PlayerName
+        {
+            get
+            {
+                return playername.Text;
+            }
+        }
+
+        private bool _playerTurn;
+        [Description("Is it this player's turn?"), Category("O'Neillo")]
+        public bool PlayerTurn
+        {
+            get
+            {
+                return _playerTurn;
+            }
+            set
+            {
+                _playerTurn = value;
+                CurrentPlayer.Visible = value;
+            }
+        }
+
+        public Playerinfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
